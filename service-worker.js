547949e2360d8c8b6b98b7cedd09558943de144a
nytyn78 <dr.nitinagarwal@gmail.com>
2026-03-12 07:49:24 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'health-tracker-v2.22j';
+const CACHE = 'health-tracker-v2.22k';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
